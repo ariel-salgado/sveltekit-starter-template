4d0641647871bb13d4d33f684033f2f710758ae3
Ariel Salgado <ariel.salgado.acevedo@gmail.com>
2025-02-02 21:51:39 -0300

    feat: using vite preprocessor with script true to enable all typescript features

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,17 @@
 import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: vitePreprocess({ script: true }),
+
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
 	},
+
 	compilerOptions: {
-		modernAst: true
-	}
+		modernAst: true,
+	},
 };
 
 export default config;
